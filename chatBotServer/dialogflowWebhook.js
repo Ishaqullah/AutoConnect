72b012b18eee7640f8f71c9c153944a2c70f87e3
Ishaqullah <ishaq.siddiqui1401@gmail.com>
2023-12-04 16:15:17 +0500

    chatbot doing negotiation

--- chatBotServer/dialogflowWebhook.js
+++ chatBotServer/dialogflowWebhook.js
@@ -6,6 +6,8 @@ const port = 3002;
 
 app.use(bodyParser.json());
 
+let discountRequests = 0; // Counter to track discount requests
+let prevDiscount=0;
 app.post("/dialogflow-webhook", async (req, res) => {
   try {
     // Fetch carDetails from server.js
@@ -15,12 +17,41 @@ app.post("/dialogflow-webhook", async (req, res) => {
     const receivedCarDetails = carDetailsResponse.data;
 
     const intentName = req.body.queryResult.intent.displayName; // Extract intent name from the request
-    let responseMessage=`Here are the details for the car you are looking for \n Name:${receivedCarDetails.name} \n  Location:${receivedCarDetails.location} \n Year:${receivedCarDetails.year} \n Mileage:${receivedCarDetails.fueltype} `;    
+    let responseMessage = `Here are the details for the car you are looking for \n Name:${receivedCarDetails.name} \n  Location:${receivedCarDetails.location} \n Year:${receivedCarDetails.year} \n Mileage:${receivedCarDetails.fueltype}`;
     let fulfillmentMessage = "";
 
     // Your logic to construct the fulfillment message using receivedCarDetails and intentName
     if (intentName === "Request-Car-Details") {
       fulfillmentMessage = responseMessage;
+    } else if (intentName === "negotitation-intent") {
+      if (discountRequests > 2) {
+        
+        const minPrice = 5000; // Define your minimum price here
+        let currentPrice = parseFloat(receivedCarDetails.price);
+        if(prevDiscount!=0){
+          currentPrice=prevDiscount;
+        }
+        // console.log(currentPrice);
+        let discountedPrice = currentPrice - (currentPrice * (Math.random() * (0.1 - 0.05) + 0.05)); // Generate a random discount price between 5% to 10%
+        discountedPrice = Math.max(minPrice, discountedPrice); // Ensuring the discounted price is greater than the minPrice
+        
+        fulfillmentMessage = `We understand your interest! The special discounted price for this car is $${discountedPrice.toFixed(2)}. Are you interested in this offer?`;
+        discountRequests=0;
+        prevDiscount=discountRequests;
+      } else {
+        // console.log("hello");
+        // Resistance messages to persuade users to purchase at the same price
+        const resistanceMessages = [
+          "This car is worth every penny for its incredible features and quality.",
+          "The value this car provides at its current price is unparalleled.",
+          "You won't find a better deal elsewhere for a car of this caliber.",
+          // Add more resistance messages as needed
+        ];
+        const randomResistanceMessage = resistanceMessages[Math.floor(Math.random() * resistanceMessages.length)];
+        fulfillmentMessage = randomResistanceMessage;
+        
+      }
+      discountRequests++;
     } else {
       fulfillmentMessage = "Default fulfillment message";
     }
