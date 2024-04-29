51559430e1025d2caf6db6120dcffc0c708ea9ae
Ishaqullah <ishaq.siddiqui1401@gmail.com>
2024-04-29 05:18:46 +0500

    fixes

---
+++ adminPortal/src/views/icons/Icons.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PageContainer from 'src/components/container/PageContainer';
+import DashboardCard from '../../components/shared/DashboardCard';
+import { ChatEngine } from 'react-chat-engine'
+
+const Icons = () => {
+
+  return (
+    <PageContainer title="ChatBox" description="this is ChatBox">
+      <DashboardCard title="ChatBox">
+      <ChatEngine
+      publicKey={'4d20f39b-c504-4897-b3ae-a1a6f00c2589'}
+      userName={'Ishaq'}
+      userSecret={'Ishaq'}
+    />
+      </DashboardCard>
+    </PageContainer>
+  );
+};
+
+export default Icons;
