b2c8d3f7726fb35d2149b333b1a4e142e4b64412
Ishaqullah <ishaq.siddiqui1401@gmail.com>
2024-05-08 01:57:27 +0500

    similar ads

--- client/src/components/SimilarAds.js
+++ client/src/components/SimilarAds.js
@@ -1,11 +1,32 @@
 import React, { useEffect, useState } from 'react'
 import { supabase } from '../lib/supabase';
-const SimilarAds = () => {
- 
+import axios from 'axios';
+import CarAdsGrid from './CarAdsGrid';
 
+const SimilarAds = ({similarAds}) => {
+  const [vehicles,setVehicles]=useState([]);
 
+ useEffect(() => {
+    const fetchVehicle = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:5278/advertises/getVehicles/${similarAds[0].advertise_id}`
+        );
+        setVehicles(response.data);
+      } catch (error) {
+        console.log("Error fetching vehicle", error);
+      } 
+    };
+
+    fetchVehicle();
+  }, []);
+  console.log(vehicles);
   return (
-    <div>SimilarAds</div>
+    <div>
+      {/* <CarAdsGrid
+            carAds={vehicles}
+          /> */}
+    </div>
   )
 }
 
